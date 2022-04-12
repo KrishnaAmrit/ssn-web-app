f2287784ed8f763ecd147032b60858e525c1d1bb
ak203@oops@gmail.com <ak203oops@gmail.com>
2022-04-12 15:06:22 +0530

    mation cross orgin

--- src/main/java/com/agamiit/controller/ContactRestcontroller.java
+++ src/main/java/com/agamiit/controller/ContactRestcontroller.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +21,7 @@ import com.agamiit.services.ContactServiceImpl;
 
 @RestController
 //@RequestMapping(value = "/contact")
+@CrossOrigin
 public class ContactRestcontroller {
 	@Autowired
 	private ContactServiceImpl contactServiceImpl;
